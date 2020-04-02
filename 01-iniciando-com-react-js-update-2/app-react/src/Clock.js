4aa2c5dbc838df3fced5c548de3dc23ccf703fdc
Rafael Barros <rafaelbarros.df@gmail.com>
2020-04-02 03:43:15 -0300

    :construction: JSX como usar

---
+++ 01-iniciando-com-react-js-update-2/app-react/src/Clock.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Clock extends Component {
+    render() {
+        const returnClock = () => {
+            return new Date()
+                .toLocaleString('en-US', {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    hour12: true
+                })
+        }
+        const color = 'red';   
+        return (
+            <h1 className={color}>
+                {returnClock()}
+            </h1>
+        );
+    }
+}
--- 01-iniciando-com-react-js-update-2/app-react/src/index.js
+++ 01-iniciando-com-react-js-update-2/app-react/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Hello from './Hello';
+import Clock from './Clock';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Clock />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
