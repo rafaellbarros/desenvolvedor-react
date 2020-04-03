7d0ca19bc469ec3c06da167456cb0e508a6e8ad6
Rafael Barros <rafaelbarros.df@gmail.com>
2020-04-03 02:59:34 -0300

    :construction: Entendendo props

--- 01-iniciando-com-react-js-update-2/app-react/src/index.js
+++ 01-iniciando-com-react-js-update-2/app-react/src/index.js
@@ -5,8 +5,9 @@ import App from './App';
 import Hello from './Hello';
 import Clock from './Clock';
 import * as serviceWorker from './serviceWorker';
+import Props from './Props';
 
-ReactDOM.render(<Clock />, document.getElementById('root'));
+ReactDOM.render(<Props attr="span" attr2="VALUE2" attr3="VALUE3" />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
