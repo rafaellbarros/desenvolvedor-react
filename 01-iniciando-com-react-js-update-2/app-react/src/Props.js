10adbd5cf1b12b6f8ab3d049411349e7245b6854
Rafael Barros <rafaelbarros.df@gmail.com>
2020-04-03 03:11:39 -0300

    :construction: Praticando props

--- 01-iniciando-com-react-js-update-2/app-react/src/Props.js
+++ 01-iniciando-com-react-js-update-2/app-react/src/Props.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Panel from './Panel';
 
 export default class Props extends Component {
 
@@ -13,6 +14,8 @@ export default class Props extends Component {
         return (
             <div>
                 { attr === 'p' ? <p>PROPS</p> : <h1>PROPS</h1>}
+                <Panel headercolor="red" bordercolor="blue" headertext="This is a hreader" data="School of Net" />
+                <Panel headercolor="gray" bordercolor="purple" headertext="This is a hreader" data="School of Net" />
             </div>
         );
     }
