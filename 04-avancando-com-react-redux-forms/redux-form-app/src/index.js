b8d57035f8f7322aea63338153f05d0673714ef1
Rafael Barros <rafaelbarros.df@gmail.com>
2020-04-18 15:04:31 -0300

    :construction: :beers: Criando app e configurando redux redux-form

--- 04-avancando-com-react-redux-forms/redux-form-app/src/index.js
+++ 04-avancando-com-react-redux-forms/redux-form-app/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import cfgStore from './store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />
+const store = cfgStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
   ,document.getElementById('root')
 );
 
