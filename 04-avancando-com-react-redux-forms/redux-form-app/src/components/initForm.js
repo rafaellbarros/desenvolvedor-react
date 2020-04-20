08e4590e01a1dfbc5f6b6f75c574c4fef5527f23
Rafael Barros <rafaelbarros.df@gmail.com>
2020-04-19 21:52:57 -0300

    :construction: Trabalhando com state

--- 04-avancando-com-react-redux-forms/redux-form-app/src/components/initForm.js
+++ 04-avancando-com-react-redux-forms/redux-form-app/src/components/initForm.js
@@ -1,31 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { getInitialFetch } from './../actions';
 
-// Component
-const InitForm = ({ onClick, onChangeName }) => {
+class InitForm extends Component {
+  
+  componentDidMount() {
+    this.props.fetchInitial();
+  }
 
+  render() {
+    
+    const { onClick, onChangeName } = this.props;
 
     return (
-        <form action="">
-          <div className="form-group">
-            <label htmlFor="name">Enter your name</label>
-            <Field 
-              name="name"
-              placeholder="Enter your name"
-              type="text"
-              component="input"
-              className="form-control"
-              onChange={onChangeName}>
-              </Field>
-          </div>
-          <div className="form-group">
-            <button type="button" className="btn btn-primary" onClick={onClick}>Send</button>
-          </div>
-        </form>
+      <form action="">
+        <div className="form-group">
+          <label htmlFor="name">Enter your name</label>
+          <Field 
+            name="name"
+            placeholder="Enter your name"
+            type="text"
+            component="input"
+            className="form-control"
+            onChange={onChangeName}>
+            </Field>
+        </div>
+        <div className="form-group">
+          <button type="button" className="btn btn-primary" onClick={onClick}>Send</button>
+        </div>
+      </form>
     )
-  };
-  
-  export default reduxForm({
-    form: 'initForm'
-  })(InitForm);
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    initialValues: state.name.data
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchInitial: () => dispatch(getInitialFetch())
+  }
+}
+
+export default connect(
+  mapStateToProps, mapDispatchToProps
+)(reduxForm({
+  form: 'initForm',
+  enableReinitialize: true
+})(InitForm));
   
