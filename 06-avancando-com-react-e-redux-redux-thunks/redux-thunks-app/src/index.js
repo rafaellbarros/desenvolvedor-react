15fe8bd62fc42fd3f74499b2935e26096003b9e9
Rafael Barros <rafaelbarros.df@gmail.com>
2020-05-27 01:37:43 -0300

    :contruction: Criando app e configurando redux

--- 06-avancando-com-react-e-redux-redux-thunks/redux-thunks-app/src/index.js
+++ 06-avancando-com-react-e-redux-redux-thunks/redux-thunks-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
+import cfgStore from './store';
+
+const store = cfgStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
+, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
